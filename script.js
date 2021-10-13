21fb7812163e3623bc35a17290c21ad44a2c8df7
Kaushal Kishor Sharma <kaushalkishor10702@gmail.com>
2021-10-13 23:31:50 +0530

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+function functionOne() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+
+
+  function functionTwo() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+  }
+  
